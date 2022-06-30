ca50cc8ca7a8a357c109ef8bd95b1490d845f89e
GogorPutra <12050112140@students.uin-suska.ac.id>
2022-06-30 11:07:10 +0700

    commit

---
+++ hostel/admin/includes/header.php
@@ -0,0 +1,13 @@
+<div class="brand clearfix">
+		<a href="#" class="logo" style="font-size:16px;">Manajemen Sistem Hotel</a>
+		<span class="menu-btn"><i class="fa fa-bars"></i></span>
+		<ul class="ts-profile-nav">
+			<li class="ts-account">
+				<a href="#"><img src="img/avatar.jpg" class="ts-avatar hidden-side" alt=""> AKUN <i class="fa fa-angle-down hidden-side"></i></a>
+				<ul>
+					<li><a href="admin-profile.php">Akun Saya</a></li>
+					<li><a href="logout.php">Logout</a></li>
+				</ul>
+			</li>
+		</ul>
+	</div>
---
+++ hostel/admin/includes/sidebar.php
@@ -0,0 +1,24 @@
+<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Main</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+					<li><a href="#"><i class="fa fa-files-o"></i> Kursus</a>
+					<ul>
+						<li><a href="add-courses.php">Tambah Kursus</a></li>
+						<li><a href="manage-courses.php">Kelola Kursus</a></li>
+					</ul>
+				</li>
+					<li><a href="#"><i class="fa fa-desktop"></i> Kamar</a>
+					<ul>
+						<li><a href="create-room.php">Tambah Kamar</a></li>
+						<li><a href="manage-rooms.php">Kelola Kamar</a></li>
+					</ul>
+				</li>
+
+				<li><a href="registration.php"><i class="fa fa-user"></i>Registrasi</a></li>
+				<li><a href="manage-students.php"><i class="fa fa-users"></i>Kelola Siswa </a></li>
+				<li><a href="access-log.php"><i class="fa fa-file"></i>User Access logs</a></li>
+
+			
+		</nav>
